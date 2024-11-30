1cd2d5e73b5da9db30e9e0442891043211777b96
Kamran Mohammed <kamranabdulbasith@gmail.com>
2024-11-30 16:56:24 +0530

    test1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/hello", (req, res) => res.send("Hello World!"));
+app.get("/helloo", (req, res) => res.send("Hello World!"));
 app.listen(3000, () => console.log("Server running on port 3000"));

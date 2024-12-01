b838b4012d749dd5dcb9caca1372d855a9d29bd4
Kamran Mohammed <kamranabdulbasith@gmail.com>
2024-12-01 08:27:21 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ app.use(express.json());
 app.use(middleware);
 
 // Routes
-app.get("/helloHiii", (req, res) => res.send("Hello World!"));
+app.get("/helloHiii", (req, res) => res.send("Hello hi World!"));
 app.get("/hello", (req, res) => res.send("Hello World!"));
+app.get("/all", (req, res) => res.send("Hello all!"));
 
 // Start the server
 const PORT = 3000;
